69d1bf86117a368624776caee0981d7db9e72bc5
1ClickPass <36980378+guanzhenbin@users.noreply.github.com>
2025-05-22 16:38:07 +0800

    libbox: expose setup options

---
+++ experimental/libbox/setup.go
@@ -0,0 +1,12 @@
+package libbox
+
+import cb "github.com/sagernet/sing-box/experimental/commonbox"
+
+// SetupOptions defines parameters for setting up global paths and user info.
+type SetupOptions = cb.SetupOptions
+
+// Setup initializes package-level variables for base, working, and temp paths.
+// It delegates to experimental/commonbox.Setup for the actual implementation.
+func Setup(options *SetupOptions) error {
+	return cb.Setup(options)
+}
