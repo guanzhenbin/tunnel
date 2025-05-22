88f15dae5075461e3d8b7a7e51ca5eb194c58c11
1ClickPass <36980378+guanzhenbin@users.noreply.github.com>
2025-05-22 15:25:36 +0800

    Define StringIterator locally

--- experimental/libbox/string_iterator_alias.go
+++ experimental/libbox/string_iterator_alias.go
@@ -1,5 +1,11 @@
 package libbox
 
-import cb "github.com/sagernet/sing-box/experimental/commonbox"
-
-type StringIterator = cb.StringIterator
+// StringIterator defines a simple iterator over strings.
+//
+// It matches the commonbox.StringIterator interface so implementations of
+// that interface automatically satisfy this one as well.
+type StringIterator interface {
+	Len() int32
+	HasNext() bool
+	Next() string
+}
